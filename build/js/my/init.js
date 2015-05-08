09fade93fbb8b99ce75c35d334e5af4359e66064
flipmotion <flipmotions@gmail.com>
2015-05-09 02:15:11 +0300

    slider
    UPD deploy
    ver. 1.0 alpha

---
+++ build/js/my/init.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$('.owl-carousel').owlCarousel({
+		loop:true,
+		margin:0,
+		nav:true,
+		dots:false,
+		items:1,
+		navText: [
+		  "<i class='my-arrow-left'></i>",
+		  "<i class='my-arrow-right'></i>"
+		]
+	});
+});
